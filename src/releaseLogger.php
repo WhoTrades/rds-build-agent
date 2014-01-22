4b70aef849b019855498cfd06a98f0da38ee110e
Mikhail Konyukhov <mk@whotrades.net>
2014-01-22 09:24:49 +0400

    [*] url to production

--- src/releaseLogger.php
+++ src/releaseLogger.php
@@ -3,7 +3,7 @@ $app = $argv[1];
 $version = $argv[2];
 $action = $argv[3];
 
-$url = 'http://phplogs.mk.whotrades.net/releases/json/?app='.$app.'&action='.$action.'&version='.$version;
+$url = 'http://phplogs.whotrades.net/releases/json/?app='.$app.'&action='.$action.'&version='.$version;
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,            $url );
