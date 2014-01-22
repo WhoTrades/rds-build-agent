a5765f84931dd61c4a903923b2448fb345fd6976
Mikhail Konyukhov <mk@whotrades.net>
2014-01-22 09:23:31 +0400

    [*] new api

--- src/releaseLogger.php
+++ src/releaseLogger.php
@@ -3,23 +3,11 @@ $app = $argv[1];
 $version = $argv[2];
 $action = $argv[3];
 
-$url = 'http://api.whotrades.com/api/systems/*/rpc/json/releases';
-$body = '{
-   "method":"getPhpLogsSystem.getReleaseModel.saveAction",
-   "params":[
-      "' . $app . '",
-      "' . $action . '",
-      "' . $version . '"
-   ],
-   "jsonrpc":"2.0"
-}';
-
+$url = 'http://phplogs.mk.whotrades.net/releases/json/?app='.$app.'&action='.$action.'&version='.$version;
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,            $url );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt($ch, CURLOPT_POST,           1 );
-curl_setopt($ch, CURLOPT_POSTFIELDS,     $body );
-curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: text/json'));
+curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: text/plain'));
 
 $result=curl_exec ($ch);
