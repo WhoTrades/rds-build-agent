38ee036ae321fc62e4c5a53ee14c010304dc997e
Artem Naumenko <entsupml@gmail.com>
2014-10-03 14:57:00 +0400

    [*] Improved script mongo_restore.sh

--- src/misc/tools/bash/mongo_restore.sh
+++ src/misc/tools/bash/mongo_restore.sh
@@ -20,27 +20,9 @@ COLS=`ssh -q $MONGOSERVER "ls /var/backup/${FROMDB}/"`
 for COL in $COLS; do
     COL=`echo $COL | sed -e 's/\.json\.gz//'`
     echo "Restore collection: $COL"
-    ssh -q $MONGOSERVER "zcat /var/backup/${FROMDB}/${COL}.json.gz | mongoimport -d ${DESTDB} --collection ${COL}" &
-done
-
-
-jobs -p -l > /tmp/mongo_restore.txt
-
-while read -r -a job; do
-    pid=${job[1]}
-
-    wait $pid;
+    ssh -q $MONGOSERVER "zcat /var/backup/${FROMDB}/${COL}.json.gz | mongoimport -d ${DESTDB} --collection ${COL}"
     if [ $? != "0" ]; then
-       let "FAIL+=1"
-       FAILED+=("${job[3]} ${job[4]}")
+        echo "Failed to import $MONGOSERVER $COL"
+        exit 1
     fi
-done < /tmp/mongo_restore.txt
-
-echo "FINISHED"
-
-for ix in ${!FAILED[*]}
-do
-    printf "Failed command:   %s\n" "${FAILED[$ix]}"
 done
-
-exit FAIL_COUNT
