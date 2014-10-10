7a871882aac2cd4f7a640e1ee05b7c4acb4f3e4d
Artem Naumenko <entsupml@gmail.com>
2014-10-10 17:24:06 +0400

    [+] Added HardMigration tool to cron config #WTA-69

--- src/misc/cron/ServiceDeployProdTL1.php
+++ src/misc/cron/ServiceDeployProdTL1.php
@@ -17,6 +17,8 @@ class ServiceDeployProdTL1
             new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_Use::getToolCommand(['--max-duration=60'], $verbosity=3), 1), '* * * * *'),
             new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_Killer::getToolCommand(['--max-duration=60'], $verbosity=3), 3), '* * * * *'),
             new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_Migration::getToolCommand(['--max-duration=60'], $verbosity=3), 3), '* * * * *'),
+            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_HardMigration::getToolCommand([], $verbosity=3), 1), '* * * * *'),
+            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_HardMigrationProxy::getToolCommand([], $verbosity=3), 1), '* * * * *'),
             new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_GarbageCollector::getToolCommand([], $verbosity=3), 86400), '* * * * *'),
         ];
 
