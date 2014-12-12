0017828782d88eaa292e034af68d3d877a89414a
Artem Naumenko <entsupml@gmail.com>
2014-12-12 16:24:01 +0300

    [-] fix typo #WTA-96

--- src/misc/cron/ServiceDeployProdTL1.php
+++ src/misc/cron/ServiceDeployProdTL1.php
@@ -23,7 +23,6 @@ class ServiceDeployProdTL1
                 new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_HardMigrationProxy::getToolCommand(['--max-duration=300'], $verbosity=3), 0), '* * * * *'),
                 new CronCommand(\Cronjob_Tool_Deploy_GarbageCollector::getToolCommand(['--dry-run'], $verbosity=3), '20 0 * * *'),
                 new CronCommand(new PeriodicCommand(\Cronjob_Tool_Maintenance_ToolRunner::getToolCommand(['--max-duration=60'], $verbosity=3), 0), '* * * * *'),
-                new CronCommand(\Cronjob_Tool_JiraMergeTasks::getToolCommand([], $verbosity=3), '* * * * *'),
             ],
             $this->getGitMergeTasks($this->mergeInstanceCount)
         );
