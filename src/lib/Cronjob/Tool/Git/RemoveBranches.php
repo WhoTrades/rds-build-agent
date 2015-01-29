42a29c6abd041f29fa30f0ae0ef8f735d0813ad6
Artem Naumenko <an@whotrades.org>
2015-01-29 12:00:46 +0300

    [+] Added skipping removing branches if there are commits not merged to master #WTA-151

--- src/lib/Cronjob/Tool/Git/RemoveBranches.php
+++ src/lib/Cronjob/Tool/Git/RemoveBranches.php
@@ -34,6 +34,7 @@ class Cronjob_Tool_Git_RemoveBranches extends RdsSystem\Cron\RabbitDaemon
      */
     public function run(\Cronjob\ICronjob $cronJob)
     {
+        ini_set('memory_limit', '1G');
         $model  = $this->getMessagingModel($cronJob);
         $instance = $cronJob->getOption('instance');
         $model->readDropBranches(false, function(\RdsSystem\Message\Merge\DropBranches $task) use ($model ,$instance) {
@@ -41,17 +42,48 @@ class Cronjob_Tool_Git_RemoveBranches extends RdsSystem\Cron\RabbitDaemon
 
             $this->debugLogger->message("Removing $branch");
 
+            $this->commandExecutor = new CommandExecutor($this->debugLogger);
 
             $dir = Cronjob_Tool_Git_Merge::fetchRepositories($this->debugLogger, $instance);
 
-            $this->commandExecutor = new CommandExecutor($this->debugLogger);
-
-
-            $cmd = "(cd $dir; node git-tools/alias/git-all.js '(git ls-remote --exit-code origin refs/heads/$branch 2>&1 > /dev/null; if [ $? -eq 2 ]; then exit 0; else git push origin :$branch; fi)')";
+            $cmd = "(cd $dir; node git-tools/alias/git-all.js git fetch)";
             $this->commandExecutor->executeCommand($cmd);
 
+            $this->commandExecutor = new CommandExecutor($this->debugLogger);
+
+            $skippedRepositories = [];
+
+            $list = glob("$dir/*");
+            foreach ($list as $repoDir) {
+                $this->debugLogger->info("Processing $repoDir");
+                $cmd = "(cd $repoDir; git ls-remote --exit-code origin refs/heads/$branch)";
+                try {
+                    $this->commandExecutor->executeCommand($cmd);
+                } catch (\RdsSystem\lib\CommandExecutorException $e) {
+                    if ($e->getCode() == 2) {
+                        //an: ветка уже была удалена или не была создана, игнорируем такую ситуацию
+                        $this->debugLogger->info("Branch $branch not exists at $repoDir, skip it");
+                        continue;
+                    }
+                    throw $e;
+                }
+
+                $cmd = "(cd $repoDir; git log origin/$branch ^origin/master --pretty=format:'%H [%ad] (%an) %s')";
+                $output = trim($this->commandExecutor->executeCommand($cmd));
+                if (!$output) {
+                    $this->debugLogger->info("Removing branch $branch as $repoDir, all commits exists at master branch");
+                    //an: Удаляем ветку, все комиты есть в мастере
+                    $cmd = "(cd $repoDir; git push origin :$branch)";
+                    $this->commandExecutor->executeCommand($cmd);
+                } else {
+                    //an: есть комиты, не смерженные в мастер. Такие ветки мы не удаляем, но оповещаем RDS о них
+                    $this->debugLogger->info("Skip removing branch $branch as $repoDir, some commits not at master: $output");
+                    $skippedRepositories[preg_replace('~^.*/~' ,'', $repoDir)] = $output;
+                }
+            }
+
             $this->debugLogger->debug("Sending reply with branch=$branch...");
-            $model->sendDroppedBranches(new Message\Merge\DroppedBranches($branch));
+            $model->sendDroppedBranches(new Message\Merge\DroppedBranches($branch, $skippedRepositories));
 
             $this->debugLogger->message("Branch was removed, task accepted");
             $task->accepted();
