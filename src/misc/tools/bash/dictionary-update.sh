7fc700ee7562371b4ce39740a185b5eb289aa5cd
Artem Naumenko <entsupml@gmail.com>
2015-06-16 15:54:04 +0300

    [-] bugfix of dictionary-update.sh

--- src/misc/tools/bash/dictionary-update.sh
+++ src/misc/tools/bash/dictionary-update.sh
@@ -1,10 +1,15 @@
-count=`ps -Af|grep $0|grep -v grep|wc -l`
+count=`ps -Af|grep $0|grep -v grep|grep -v sudo|wc -l`
 if [ $count -gt 2 ]; then
-    echo `ps -Af|grep $0|grep -v grep`
+    ps -Af|grep $0|grep -v grep|grep -v sudo
     echo "Double run"
     exit 1;
 fi
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-yes | env PGHOST=pg-0-1.comon.local env PGDATABASE=comon1 sudo sh $DIR/update-data-static.sh;
-sudo -u www-data php /home/dev/dev/comon/misc/tools/runner.php --tool=CacheInvalidator -v --memcache;
+
+if [ $1 -ne "" ]; then
+    yes | env PGHOST=pg-0-1.comon.local env PGDATABASE=comon1 sudo sh $DIR/update-data-static.sh;
+    sudo -u www-data php /home/dev/dev/comon/misc/tools/runner.php --tool=CacheInvalidator -v --memcache;
+fi
+
 sudo -u www-data php /home/dev/dev/comon/misc/tools/runner.php --tool=DictionaryLoaderGetText --path=/tmp/Dictionary/locale -vvv;
+
