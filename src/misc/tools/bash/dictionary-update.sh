e7ff990034751fa58b9c0ca075029a62bafb7571
Artem Naumenko <entsupml@gmail.com>
2015-07-07 10:18:02 +0300

    [-] Bugfix at deployment dictionary to tst env #WTA-373

--- src/misc/tools/bash/dictionary-update.sh
+++ src/misc/tools/bash/dictionary-update.sh
@@ -1,6 +1,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ $1 -ne "" ]; then
+if [ $1 -ne "0" ]; then
     yes | env PGHOST=pg-0-1.comon.local env PGDATABASE=comon1 sudo sh $DIR/update-data-static.sh;
     sudo -u www-data php /home/dev/dev/comon/misc/tools/runner.php --tool=CacheInvalidator -v --memcache;
 fi
