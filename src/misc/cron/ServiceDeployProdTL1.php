d952f7d4184782d9257969f6541ef87c351cc41c
Artem Naumenko <entsupml@gmail.com>
2015-08-26 17:21:43 +0300

    [*] Switched to cron-n.d at service-deploy #WTA-417

--- src/misc/cron/ServiceDeployProdTL1.php
+++ src/misc/cron/ServiceDeployProdTL1.php
@@ -16,22 +16,22 @@ class ServiceDeployProdTL1
     {
         $allCommands = [
             new Comment("Сборка"),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_Deploy::getToolCommand(['--max-duration=60'], $verbosity=3), 5), '* * * * *', 'deploy_deploy'),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_Use::getToolCommand(['--max-duration=60'], $verbosity=3), 1), '* * * * *', 'deploy_use'),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_Killer::getToolCommand(['--max-duration=60'], $verbosity=3), 3), '* * * * *', 'deploy_killer'),
+            new CronCommand(\Cronjob_Tool_Deploy_Deploy::getToolCommand(['--max-duration=60'], $verbosity=3), '* * * * * *', 'deploy_deploy'),
+            new CronCommand(\Cronjob_Tool_Deploy_Use::getToolCommand(['--max-duration=60'], $verbosity=3), '* * * * * *', 'deploy_use'),
+            new CronCommand(\Cronjob_Tool_Deploy_Killer::getToolCommand(['--max-duration=60'], $verbosity=3), '* * * * * *', 'deploy_killer'),
 
             new Comment("Миграции"),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_Migration::getToolCommand(['--max-duration=60'], $verbosity=3), 3), '* * * * *', 'deploy_migration'),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_HardMigration::getToolCommand(['--max-duration=60'], $verbosity=3), 1), '* * * * *', 'deploy_hard_migration'),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Deploy_HardMigrationProxy::getToolCommand(['--max-duration=300'], $verbosity=3), 0), '* * * * *', 'deploy_hard_migration_proxy'),
+            new CronCommand(\Cronjob_Tool_Deploy_Migration::getToolCommand(['--max-duration=60'], $verbosity=3), '* * * * * *', 'deploy_migration'),
+            new CronCommand(\Cronjob_Tool_Deploy_HardMigration::getToolCommand(['--max-duration=60'], $verbosity=3), '* * * * * *', 'deploy_hard_migration'),
+            new CronCommand(\Cronjob_Tool_Deploy_HardMigrationProxy::getToolCommand(['--max-duration=300'], $verbosity=3), '* * * * * *', 'deploy_hard_migration_proxy'),
 
             new Comment("Обслуживание, удаление мусора и т.д."),
             new CronCommand(\Cronjob_Tool_Deploy_GarbageCollector::getToolCommand([], $verbosity=3), '20 0 * * *', 'DeployGarbageCollector'),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Maintenance_ToolRunner::getToolCommand(['--max-duration=60'], $verbosity=3), 0), '* * * * *', 'deploy_maintenance_runner'),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Git_RemoveBranches::getToolCommand(['--max-duration=60 --instance=1'], $verbosity=3), 0), '* * * * *', 'deploy_remove_branches'),
+            new CronCommand(\Cronjob_Tool_Maintenance_ToolRunner::getToolCommand(['--max-duration=60'], $verbosity=3), '* * * * * *', 'deploy_maintenance_runner'),
+            new CronCommand(\Cronjob_Tool_Git_RemoveBranches::getToolCommand(['--max-duration=60 --instance=1'], $verbosity=3), '* * * * * *', 'deploy_remove_branches'),
 
             new Comment("Git merge"),
-            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Git_Merge::getToolCommand(['--max-duration=60', '--instance=0'], $verbosity=2), 0), '* * * * *', 'deploy_git_merge'),
+            new CronCommand(\Cronjob_Tool_Git_Merge::getToolCommand(['--max-duration=60', '--instance=0'], $verbosity=2),'* * * * * *', 'deploy_git_merge'),
 //            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Git_Merge::getToolCommand(['--max-duration=60', '--instance=0 --allowed-branches=develop'], $verbosity=2), 0), '* * * * *', 'deploy_git_merge-develop'),
 //            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Git_Merge::getToolCommand(['--max-duration=60', '--instance=1 --allowed-branches=staging'], $verbosity=2), 0), '* * * * *', 'deploy_git_merge-staging'),
 //            new CronCommand(new PeriodicCommand(\Cronjob_Tool_Git_Merge::getToolCommand(['--max-duration=60', '--instance=2 --allowed-branches=master'], $verbosity=2), 0), '* * * * *', 'deploy_git_merge-master'),
