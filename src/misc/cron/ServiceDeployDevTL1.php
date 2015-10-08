aafe56b9b632afd15c1169b05c5247b3c2ff7ad7
Artem Naumenko <entsupml@gmail.com>
2015-10-08 14:55:20 +0300

    [+] gencronjobs added to git for dev env #WTA-455

--- src/misc/cron/ServiceDeployDevTL1.php
+++ src/misc/cron/ServiceDeployDevTL1.php
@@ -1,10 +1,5 @@
 <?php
 class ServiceDeployDevTL1 extends ServiceDeployTestTL1
 {
-   protected function getEnv()
-    {
-        return [
-            'CRONJOB_TOOLS=/home/an/dev/services/deploy/misc/tools',
-        ];
-    }
+
 }
---
+++ src/misc/cron/ServiceDeployDevTL2.php
@@ -0,0 +1,5 @@
+<?php
+class ServiceDeployDevTL2 extends ServiceDeployTestTL2
+{
+
+}
