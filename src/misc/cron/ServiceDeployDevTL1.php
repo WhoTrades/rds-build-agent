9d0075b381c2104614fcf0b0d42598a2d041c306
Artem Naumenko <entsupml@gmail.com>
2016-03-03 10:57:51 +0300

    [+] Added function to rebuild staging branch after move ticket avay from staging #WTA-616

--- src/misc/cron/ServiceDeployDevTL1.php
+++ src/misc/cron/ServiceDeployDevTL1.php
@@ -1,5 +1,22 @@
 <?php
+/**
+ * @author Artem Naumenko
+ */
+
+use \Cronjob\ConfigGenerator\CronCommand;
+
 class ServiceDeployDevTL1 extends ServiceDeployTestTL1
 {
-
+    /**
+     * @return array
+     */
+    public function getCronConfigRows()
+    {
+        return array_merge(
+            parent::getCronConfigRows(),
+            [
+                new CronCommand(\Cronjob_Tool_PDev_Manager::getToolCommand(['--max-duration=86400'], $verbosity = 3), '* * * * * *', 'deploy_switch_branches'),
+            ]
+        );
+    }
 }
