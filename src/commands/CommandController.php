ced9cbc761a9bc08d3f64ff5c287f90f2b87d33f
Artem Naumenko <entsupml@gmail.com>
2017-08-30 10:23:43 -0400

    [*] Changed schema of removing packages, script of removing moved to RDS GUI #WTA-1281

--- src/commands/CommandController.php
+++ src/commands/CommandController.php
@@ -34,15 +34,19 @@ class CommandController extends \RdsSystem\commands\CommandController
             $this->createCommand(UseController::class, 'index', [$workerName], "deploy_use_$workerName"),
             $this->createCommand(KillerController::class, 'index', [$workerName], "deploy_killer_$workerName"),
 
+            "# Обслуживание, удаление мусора и т.д.",
+            $this->createCommand(GarbageCollectorController::class, 'index', [$workerName, 1], "deploy_garbage_collector_$workerName", '12 20 * * * *'),
+
             "# Миграции $workerName",
             $this->createCommand(MigrationController::class, 'index', [$workerName], "deploy_migration_$workerName"),
-            $this->createCommand(HardMigrationController::class, 'index', [$workerName], "deploy_hard_migration_$workerName"),
-            $this->createCommand(HardMigrationProxyController::class, 'index', ['debian'], "deploy_hard_migration_proxy_$workerName"),
 
-            "# Обслуживание, удаление мусора и т.д.",
-            $this->createCommand(GarbageCollectorController::class, 'index', ['debian'], 'deploy_garbage_collector', '12 20 * * * *'),
+            "# Работа с git (Wtflow)",
             $this->createCommand(GitDropBranchesController::class, 'index', ['debian'], 'deploy_remove_branches'),
             $this->createCommand(GitMergeController::class, 'index', ['debian'], 'deploy_git_merge'),
+
+            "# Тяжелые миграции (WhoTrades)",
+            $this->createCommand(HardMigrationController::class, 'index', [$workerName], "deploy_hard_migration_$workerName"),
+            $this->createCommand(HardMigrationProxyController::class, 'index', ['debian'], "deploy_hard_migration_proxy_$workerName"),
         ];
     }
 }
--- src/commands/GarbageCollectorController.php
+++ src/commands/GarbageCollectorController.php
@@ -15,94 +15,50 @@ use RdsSystem\Message;
 class GarbageCollectorController extends RabbitListener
 {
     /**
+     * @param string $receiverName - имя обработчика
      * @param bool $dryRun - remove packages or just print packages to remove
      */
-    public function actionIndex($dryRun = null)
+    public function actionIndex($receiverName, $dryRun = null)
     {
-        $dryRun = $dryRun ?? false;
+        $dryRun = $dryRun ?? true;
         $model  = $this->getMessagingModel();
-        $model->sendGetProjectsRequest(new Message\ProjectsRequest());
         $commandExecutor = new CommandExecutor();
 
-        $model->readGetProjectsReply(false, function (Message\ProjectsReply $message) use ($model, $commandExecutor) {
-            $toTest = array();
-            foreach ($message->projects as $project) {
-                $command = Yii::$app->params['debug'] ? "cat bash/whotrades_repo.txt" : "reprepro -b /var/www/whotrades_repo/ listmatched wheezy '{$project['name']}-*'";
-                $text = $commandExecutor->executeCommand($command);
-                if (preg_match_all('~' . $project['name'] . '-([\d.]+)~', $text, $ans)) {
-                    $versions = $ans[1];
-                    foreach ($versions as $version) {
-                        $toTest[$project['name'] . "-" . $version] = array(
-                            'project' => $project['name'],
-                            'version' => $version,
-                        );
-                    }
-                } else {
-                    Yii::info("No builds of {$project['name']} found");
-                }
-            }
-
-            $command = Yii::$app->params['debug'] ? "cat bash/whotrades_builds.txt" : "find /home/release/buildroot/ -maxdepth 1 -type d";
-            $text = $commandExecutor->executeCommand($command);
-            if (preg_match_all('~([\w-]{5,})-([\d.]{5,})~', $text, $ans)) {
-                $versions = $ans[2];
-                foreach ($versions as $key => $version) {
-                    $project = $ans[1][$key];
-                    $toTest["$project-$version"] = array(
-                        'project' => $project,
-                        'version' => $version,
-                    );
-                }
-            } else {
-                Yii::info("No builds at /home/release/buildroot/ found");
-            }
-
-            $model->sendGetProjectBuildsToDeleteRequest(new Message\ProjectBuildsToDeleteRequest($toTest));
-
-            $message->accepted();
-        });
-
-        $model->readGetProjectBuildsToDeleteReply(false, function (Message\ProjectBuildsToDeleteReply $task) use ($model, $dryRun, $commandExecutor) {
+        $model->readDropReleaseRequest($receiverName, true, function (Message\DropReleaseRequest $task) use ($model, $dryRun, $commandExecutor) {
             Yii::info("Task received: " . json_encode($task, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
             $project = $task->project;
             $version = $task->version;
 
             if (strlen($project) < 3) {
+                Yii::error("Strange project $project, skip it");
                 $task->accepted();
 
                 return;
             }
             if (strlen($version) < 3) {
+                Yii::error("Strange version $project, skip it");
                 $task->accepted();
 
                 return;
             }
+
+            // an: принимаем задачу ещё до выполнения фактического удаления, так как даже если что-то пойдет не так RDS пришлет ещё такой же пакет
+            $task->accepted();
+
             if ($dryRun) {
                 Yii::info("Fake removing $project-$version");
             } else {
-                Yii::info("Removing $project-$version");
-                if (is_dir("/home/release/buildroot/$project-$version")) {
-                    $commandExecutor->executeCommand("sudo rm -rf /home/release/buildroot/$project-$version");
-                }
-                try {
-                    $commandExecutor->executeCommand("bash bash/rsync/remove.sh $project $version");
-                } catch (CommandExecutorException $e) {
-                    if ($e->getCode() != 1) {
-                        // an: Код 1 - допустим, его игнорируем, значит просто не на всех серверах была установлена эта сборка
-                        throw $e;
-                    }
-                }
-                $commandExecutor->executeCommand("reprepro -b /var/www/whotrades_repo/ remove wheezy $project-$version");
+                $env = [
+                    'projectName' => $project,
+                    'version' => $version,
+                ];
+                $removeNewScriptFilename = "/tmp/remove-release-request" . uniqid() . ".sh";
+
+                Yii::info("Removing $project-$version, scriptFileName=$removeNewScriptFilename, env=" . json_encode($env));
+                file_put_contents($removeNewScriptFilename, $removeNewScriptFilename);
+                $commandExecutor->executeCommand("$removeNewScriptFilename 2>&1", $env);
                 $model->removeReleaseRequest(new Message\RemoveReleaseRequest($project, $version));
             }
-
-            $task->accepted();
         });
-
-        try {
-            $model->waitForMessages(null, null, 30);
-        } catch (PhpAmqpLib\Exception\AMQPTimeoutException $e) {
-            //skip
-        }
     }
 }
