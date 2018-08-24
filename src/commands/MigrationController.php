68f238c411e6b1e97f1755f3d300e11c493c549f
Anton Gorlanov <antonxacc@gmail.com>
2018-08-24 15:55:36 +0300

    [*] Move post migration handling to separate page #WTA-1732

--- src/commands/MigrationController.php
+++ src/commands/MigrationController.php
@@ -13,6 +13,9 @@ use whotrades\RdsSystem\Message;
 
 class MigrationController extends RabbitListener
 {
+    const MIGRATION_COMMAND_UP = 'up';
+    const MIGRATION_COMMAND_UP_ONE = 'up-one';
+
     /**
      * @param string $workerName
      */
@@ -20,7 +23,7 @@ class MigrationController extends RabbitListener
     {
         $model  = $this->getMessagingModel();
 
-        $model->getMigrationTask($workerName, false, function (\whotrades\RdsSystem\Message\MigrationTask $task) use ($workerName, $model) {
+        $model->getMigrationTask($workerName, false, function (Message\MigrationTask $task) use ($workerName, $model) {
             $commandExecutor = new CommandExecutor();
 
             $projectDir = "/home/release/builds/$task->project-$task->version/";
@@ -30,20 +33,51 @@ class MigrationController extends RabbitListener
                 file_put_contents($migrationUpScriptFilename, str_replace("\r", "", $task->scriptMigrationUp));
                 chmod($migrationUpScriptFilename, 0777);
 
+                $migrationCommand = $task->migrationName ? self::MIGRATION_COMMAND_UP_ONE : self::MIGRATION_COMMAND_UP;
+
                 $env = [
                     'projectName' => $task->project,
                     'type' => $task->type,
                     'projectDir' => $projectDir,
+                    'migrationCommand' => $migrationCommand,
+                    'migrationName' => $task->migrationName,
                 ];
-                $commandExecutor->executeCommand("$migrationUpScriptFilename 2>&1", $env);
+                $result = $commandExecutor->executeCommand("$migrationUpScriptFilename 2>&1", $env);
 
-                $model->sendMigrationStatus(new \whotrades\RdsSystem\Message\ReleaseRequestMigrationStatus($task->project, $task->version, $task->type, 'up'));
+                $model->sendMigrationStatus(
+                    new Message\MigrationStatus(
+                        $task->project,
+                        $task->version,
+                        $task->type,
+                        $task->migrationName,
+                        Message\MigrationStatus::STATUS_UP,
+                        $result
+                    )
+                );
             } catch (CommandExecutorException $e) {
                 Yii::error($e->getMessage());
                 Yii::info($e->output);
-                $model->sendMigrationStatus(new \whotrades\RdsSystem\Message\ReleaseRequestMigrationStatus($task->project, $task->version, $task->type, 'failed', $e->output));
+                $model->sendMigrationStatus(
+                    new Message\MigrationStatus(
+                        $task->project,
+                        $task->version,
+                        $task->type,
+                        $task->migrationName,
+                        Message\MigrationStatus::STATUS_FAILED,
+                        $e->output
+                    )
+                );
             } catch (\Exception $e) {
-                $model->sendMigrationStatus(new \whotrades\RdsSystem\Message\ReleaseRequestMigrationStatus($task->project, $task->version, $task->type, 'failed', $e->getMessage()));
+                $model->sendMigrationStatus(
+                    new Message\MigrationStatus(
+                        $task->project,
+                        $task->version,
+                        $task->type,
+                        $task->migrationName,
+                        Message\MigrationStatus::STATUS_FAILED,
+                        $e->getMessage()
+                    )
+                );
             }
 
             unlink($migrationUpScriptFilename);
