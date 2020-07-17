39e235594da08bfce08b87bf114639358b5ff3fd
Maksim Rodikov <mrodikov@corp.finam.ru>
2020-07-17 14:07:03 +0300

    [+] New DeployService #WTA-2412 [+] Unit tests

--- src/config/console.php
+++ src/config/console.php
@@ -1,5 +1,6 @@
 <?php
-use whotrades\RdsSystem\lib\ConsoleErrorHandler;
+use \yii\console\ErrorHandler;
+use \samdark\log\PsrTarget;
 
 $logger = new \whotrades\MonologExtensions\LoggerWt('main');
 $logger->pushProcessor(new \whotrades\MonologExtensions\Processor\TagCollectorProcessor());
@@ -7,14 +8,11 @@ $logger->pushProcessor(new \whotrades\MonologExtensions\Processor\TagProcessProc
 $logger->pushProcessor(new \whotrades\MonologExtensions\Processor\LoggerNameProcessor());
 $logger->pushProcessor(new \whotrades\MonologExtensions\Processor\OperationProcessor());
 
-$syslogHandler = new \whotrades\MonologExtensions\Handler\SyslogHandler('', LOG_LOCAL4, \Monolog\Logger::DEBUG);
-$syslogHandler->setFormatter(new \whotrades\MonologExtensions\Formatter\LineFormatter(null, ''));
-$logger->pushHandler($syslogHandler);
-
-$streamHandler = new \Monolog\Handler\StreamHandler("php://stdout", \Monolog\Logger::WARNING);
+$streamHandler = new \Monolog\Handler\StreamHandler("php://stdout", \Monolog\Logger::INFO);
+$streamHandler->pushProcessor(new \Monolog\Processor\PsrLogMessageProcessor());
 $streamHandler->setFormatter(new \whotrades\MonologExtensions\Formatter\LineFormatter(null, ''));
 $logger->pushHandler($streamHandler);
-
+/*
 $sentryOptions = [
     'dsn' => 'https://0e1f7a05e34544bd95a1ad42643bff95:edee09e6ecda434f9a896021d3a3459f@sentry-gw-int.dev.whotrades.net/13',
     'release' => 'test_version',
@@ -31,11 +29,11 @@ $sentryHub->bindClient($sentryClient);
 $sentryHandler = new \whotrades\MonologExtensions\Handler\SentryHandler($sentryHub, \Monolog\Logger::ERROR);
 $sentryHandler->pushProcessor(new \Monolog\Processor\PsrLogMessageProcessor());
 $logger->pushHandler($sentryHandler);
-
+*/
 $config = [
     'id' => 'service-deploy',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', ],//'sentry'
+    'bootstrap' => ['log', ],
     'controllerNamespace' => 'whotrades\RdsBuildAgent\commands',
     'aliases' => [
         '@whotrades/RdsBuildAgent' => 'src',
@@ -52,26 +50,13 @@ $config = [
             'flushInterval' => 1,
             'targets' => [
                 [
-                    'class' => 'codemix\streamlog\Target',
-                    'url' => 'php://stdout',
-                    'levels' => ['info', 'warning', 'error'],
-                    'logVars' => [],
-                    'exportInterval' => 1,
-                ],
-                [
-                    'class' => \samdark\log\PsrTarget::class,
+                    'class' => PsrTarget::class,
                     'logger' => $logger,
-                    'levels' => [\yii\log\Logger::LEVEL_ERROR, \Psr\Log\LogLevel::ERROR, \Psr\Log\LogLevel::CRITICAL, \Psr\Log\LogLevel::ALERT, \Psr\Log\LogLevel::EMERGENCY],
                 ]
             ],
         ],
-        /*'sentry' => [
-            'enabled' => false,
-            'class' => mito\sentry\Component::class,
-            'dsn' => 'https://36096034f31943d5e183555b2de11221:431c23f004608d05993c8df0ef54e096@sentry.com/1', // private DSN
-        ],*/
         'errorHandler' => array(
-            'class' => ConsoleErrorHandler::class,
+            'class' => ErrorHandler::class,
             'discardExistingOutput' => false,
         ),
     ],
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,6 @@
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-defined('LOG_LOCAL4') or define('LOG_LOCAL4', LOG_USER); // windows quickfix
-
 require_once(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../src/config/console.php');
